c22c400b1cf60b0cdf899966bb3540194d09399b
romanobrentani <brentani.v@gmail.com>
2025-03-25 14:42:11 +0100

    open menu auto

--- src/app/side-navigation/side-navigation.component.ts
+++ src/app/side-navigation/side-navigation.component.ts
@@ -1,10 +1,19 @@
-import {Component, Input} from '@angular/core';
-import {MatFormField} from '@angular/material/select';
+import {
+  AfterViewChecked, AfterViewInit,
+  Component,
+  ElementRef,
+  Input, OnChanges,
+  OnInit,
+  QueryList, SimpleChanges, ViewChild,
+  ViewChildren,
+
+} from '@angular/core';
 import {MatDrawer, MatDrawerContainer} from '@angular/material/sidenav';
 import {MatIcon} from '@angular/material/icon';
 import {MatButton, MatFabButton} from '@angular/material/button';
 import {NgIf} from '@angular/common';
 import {RouterLink} from '@angular/router';
+import {MatFormField} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-side-navigation',
@@ -23,8 +32,18 @@ import {RouterLink} from '@angular/router';
   standalone: true,
   styleUrl: './side-navigation.component.css'
 })
-export class SideNavigationComponent {
-  showFiller = false;
+export class SideNavigationComponent implements OnInit, AfterViewInit{
   @Input() showLogoutAndProfile: boolean = true;
+  @Input() showMenuContent: boolean = false;
+  @ViewChildren('drawer') menu!: QueryList<ElementRef>;
+  @ViewChild('drawer') drawer!: MatDrawer;
+
+  ngAfterViewInit() {
+    this.drawer.open(); // Öffnet den Drawer nach der Initialisierung
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
