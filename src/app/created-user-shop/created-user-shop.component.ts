19b800c6ea838f0966505d96ebb28884c0cdc520
romanobrentani <brentani.v@gmail.com>
2025-04-14 17:23:42 +0200

    update

--- src/app/created-user-shop/created-user-shop.component.ts
+++ src/app/created-user-shop/created-user-shop.component.ts
@@ -301,7 +301,7 @@ export class CreatedUserShopComponent implements OnInit, OnDestroy {
         this.experimentService.saveMentalModelExperimentExecution(this.execution).subscribe(() => {
           setTimeout(() => {
             this.loading = false;
-            this.router.navigateByUrl("/tests/"+this.experimentTest.id);
+            this.router.navigateByUrl("/tests/"+this.experimentTest.experiment.id);
             this.toasterService.success("Vielen Dank. Sie haben das Experiment erfolgreich abgeschlossen");
           }, 2000)
         });
